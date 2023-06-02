0c9ad8323bbeb41cfa5b215d28cf63ab4e5a90d1
sheldon-hung <sheldon55071@gmail.com>
2023-06-02 23:51:12 +0800

    version7

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import importlib
 flan_T5 = importlib.import_module("flan-t5")
 flan_alpaca = importlib.import_module("flan-alpaca")
 
+
 def main():
     input_text = "Write a fantasy story about a tragic knight in an apocalyptic world."
-    flan_T5.model(input_text, model_index=0)
-    flan_alpaca.model(input_text, model_index=0)
+    flan_T5.model(input_text, model_index=3)
+    # flan_alpaca.model(input_text, model_index=0)
 
 
 if __name__ == "__main__":
