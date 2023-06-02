68c5ae43da710222e96d5ab882f088134af38d0c
sheldon-hung <sheldon55071@gmail.com>
2023-06-02 22:27:54 +0800

    version5

--- flan-t5.py
+++ flan-t5.py
@@ -4,12 +4,13 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 def model(input_text, model_index):
     model_size = ["base", "small", "large", "xl"]
 
-    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-" + model_size[model_index])
+    tokenizer = T5Tokenizer.from_pretrained(
+        "google/flan-t5-" + model_size[model_index])
     model = T5ForConditionalGeneration.from_pretrained(
         "google/flan-t5-" + model_size[model_index], device_map="auto"
     )
 
-    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
     outputs = model.generate(
         input_ids,
         min_length=128,
@@ -17,5 +18,5 @@ def model(input_text, model_index):
         no_repeat_ngram_size=2,
         early_stopping=True
     )
-    
+
     print(tokenizer.decode(outputs[0]))
