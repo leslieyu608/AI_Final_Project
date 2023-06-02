475c0c2eab53d92a1fafa0d681372ab77fed68ed
sheldon-hung <125278499+sheldon-hung@users.noreply.github.com>
2023-06-02 20:39:39 +0800

    Add files via upload

--- flan-t5.py
+++ flan-t5.py
@@ -1,18 +1,19 @@
 # pip install accelerate
+from main import T5Tokenizer, T5ForConditionalGeneration
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-model_size = ["base", "small", "large", "xl"]
 
-tokenizer = T5Tokenizer.from_pretrained(
-    "google/flan-t5-" + model_size[3])
-model = T5ForConditionalGeneration.from_pretrained(
-    "google/flan-t5-" + model_size[3], device_map="auto")
+def flan_t5(input_text, model_index):
 
-input_text = "Write a fantasy story about a tragic knight in an apocalyptic world."
-input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+    tokenizer = T5Tokenizer.from_pretrained(
+        "google/flan-t5-" + model_index)
+    model = T5ForConditionalGeneration.from_pretrained(
+        "google/flan-t5-" + model_index, device_map="auto")
 
-outputs = model.generate(input_ids, min_length=128,
-                         max_new_tokens=512,
-                         no_repeat_ngram_size=2,
-                         early_stopping=True)
-print(tokenizer.decode(outputs[0]))
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+
+    outputs = model.generate(input_ids, min_length=128,
+                             max_new_tokens=512,
+                             no_repeat_ngram_size=2,
+                             early_stopping=True)
+    print(tokenizer.decode(outputs[0]))
