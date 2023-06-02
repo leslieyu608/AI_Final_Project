853625a0f7665b71fbe4b5f48f24940a3d80feb7
sheldon-hung <125278499+sheldon-hung@users.noreply.github.com>
2023-06-02 20:51:56 +0800

    Add files via upload

--- flan-t5.py
+++ flan-t5.py
@@ -3,7 +3,7 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 def flan_t5(input_text, model_index):
 
-    model_size = [base, small, large, xl]
+    model_size = ["base", "small", "large", "xl"]
 
     tokenizer = T5Tokenizer.from_pretrained(
         "google/flan-t5-" + model_size[model_index])
